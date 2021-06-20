a28bdd62162cdd75e2200cd983c2db639ec862a3
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-06-20 22:38:21 +0530

    Adding Comments

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ class TipCalculator {
         this.number = 1 ;
     }
 
+    // method to return total tip 
     totalTip = () =>{
         let totalTipAmount = this.bill + ((this.bill * this.tip)/100) ; 
         return totalTipAmount ;
     }
+
+    // method to return tip person 
     tipPerPerson = (totalTip) =>{
         let tipPerPerson = totalTip/this.number ; 
     }
-
+    // method to return bill perperson 
     billPerPerson = (total) => {
         let eachShare = total / this.number;
         return eachShare;
