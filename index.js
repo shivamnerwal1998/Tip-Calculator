951beaf28a08a19798b2aaa96daf59ba6aad5d1f
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-06-20 23:45:49 +0530

    Adding event Listners

--- index.js
+++ index.js
@@ -4,28 +4,49 @@
 */
 class TipCalculator {
 
-    constructor(){
-        this.bill = 0 ; 
-        this.tip = 0  ; 
-        this.number = 1 ;
+    constructor() {
+        this.bill = 0;
+        this.tip = 0;
+        this.number = 1;
     }
 
     // method to return total tip 
-    totalTip = () =>{
-        let totalTipAmount = this.bill + ((this.bill * this.tip)/100) ; 
-        return totalTipAmount ;
+    totalTip = () => {
+        let totalTipAmount = this.bill + ((this.bill * this.tip) / 100);
+        return totalTipAmount;
     }
 
-    // method to return tip person 
-    tipPerPerson = (totalTip) =>{
-        let tipPerPerson = totalTip/this.number ; 
+    // method to return tip per person 
+    tipPerPerson = (totalTip) => {
+        let tipPerPerson = totalTip / this.number;
     }
-    // method to return bill perperson 
+    // method to return bill per person 
     billPerPerson = (total) => {
         let eachShare = total / this.number;
         return eachShare;
+    }
+    showBill = () =>{
+        console.log(this.bill) ;
     }
 
 
 }
 
+// creating the instance of calculator 
+let calc = new TipCalculator();
+
+let bill = document.getElementById("bill");
+bill.addEventListener("change", () => {
+    calc.bill = bill.value;
+    console.log(calc.billPerPerson(calc.bill)); 
+});
+
+
+
+
+/*
+let tip = document.getElementById("tip");
+console.log( tip.value ) ;
+
+let number = document.getElementById("number");
+console.log( number.value ) ; */
