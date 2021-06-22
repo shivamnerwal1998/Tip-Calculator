ba703359650742baa29fc4c12d5a43808d2e8613
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-06-22 14:13:48 +0530

    Enabling Increment and Decrement buttons
    Enabling increment and decrement buttons

--- index.js
+++ index.js
@@ -15,43 +15,64 @@ class TipCalculator {
     amountPerPerson(action) {
         this.totalTipAmount = (Number(this.bill) + (Number(this.bill) * Number(this.tip)) / 100);
         this.totalBill = this.totalTipAmount;
-        let eachShare = this.totalBill / Number(this.number);
+        let eachShare = (this.totalBill / Number(this.number)).toFixed(2);
         let target = document.getElementById("totalBill");
-            target.style.display ="block" ;
+        if (action == "show") {
+            target.style.display = "block";
             target.innerHTML = "$" + eachShare;
+        }
+        else {
+            target.style.display = "none";
+        }
 
     }
 
     // method to display tip per person 
     tipPerPerson = (action) => {
         let tipEach = (this.bill) * Number(tip.value) / (Number(this.number) * 100);
+        tipEach = tipEach.toFixed(2);
         let target = document.getElementById("tipEach");
-            target.style.display ="block" ;
+        if (action == "show") {
+            target.style.display = "block";
             target.innerHTML = "$" + tipEach;
-          
-        
+        }
+        else {
+            display = "none";
+        }
+
+
 
     }
 
     static increment(id) {
+        let target;
         if (id === "tip") {
-            console.log("hello world")
+            target = document.getElementById("tip");
+            console.log("in tip");
+            ++(target.value);
+            // to add tip check exception 
 
         }
         else if (id === "number") {
-            console.log("hello Number");
+            target = document.getElementById("number");
+            ++(target.value);
+            inputException.numberCheck();
+            // to add tip check exception here
 
 
         }
+
 
     }
     static decrement(id) {
         if (id === "tip") {
-            console.log("hello world")
+            let target = document.getElementById("tip");
+            inputException.buttonException(id);
 
         }
         else if (id === "number") {
-            console.log("hello Number");
+            let targrt = document.getElementById("number");
+            inputException.buttonException(id);
 
 
         }
@@ -67,8 +88,10 @@ class TipCalculator {
 // creating the instance of calculator 
 let calc = new TipCalculator();
 
-// class handeling input exceptions 
+// class contgaining methods to handle input exceptions 
 class inputException {
+
+    // method to check all the exceptions of bill input feild 
     static billCheck() {
         let target = document.getElementById("bill").value;
         let line = document.getElementById("billException");
@@ -77,12 +100,16 @@ class inputException {
             line.style.color = "red";
             line.style.fontSize = "70%";
             line.innerHTML = "Bill Should be +ve in value ";
+            calc.amountPerPerson("hide");
+            calc.tipPerPerson("hide");
         }
         else if (target.length == 0) {
             line.style.display = "block";
             line.style.color = "red";
             line.style.fontSize = "70%";
             line.innerHTML = "Enter a valid number";
+            calc.amountPerPerson("hide");
+            calc.tipPerPerson("hide");
 
         }
         else {
@@ -94,39 +121,83 @@ class inputException {
         }
 
     }
+
+    //method to handle all exceptions of Number input feild  
     static numberCheck() {
         let target = document.getElementById("number").value;
         let line = document.getElementById("numberException");
         if (Number(target) == 0) {
             line.style.display = "block";
             line.style.color = "red";
-            line.style.fontSize = "70%";
-            line.innerHTML = "Number is greater than Zero";
+            line.style.fontSize = "80%";
+            line.innerHTML = "Number should be greater than Zero";
+            calc.amountPerPerson("hide");
+            calc.tipPerPerson("hide");
         }
         else if (Number(target) < 0) {
             line.style.display = "block";
             line.style.color = "red";
-            line.style.fontSize = "70%";
+            line.style.fontSize = "80%";
             line.innerHTML = "Number Should be +ve in value ";
+            calc.amountPerPerson("hide");
+            calc.tipPerPerson("hide");
         }
         else if (target.length == 0) {
             line.style.display = "block";
             line.style.color = "red";
-            line.style.fontSize = "70%";
+            line.style.fontSize = "80%";
             line.innerHTML = "Enter a valid number";
+            calc.amountPerPerson("hide");
+            calc.tipPerPerson("hide");
 
         }
         else if (!(Number.isInteger(Number(target)))) {
             line.style.display = "block";
             line.style.color = "red";
-            line.style.fontSize = "70%";
+            line.style.fontSize = "80%";
             line.innerHTML = "Number should be a positive Integer";
+            calc.amountPerPerson("hide");
+            calc.tipPerPerson("hide");
 
         }
 
         else {
             line.style.display = "none";
             calc.number = number.value;
+            calc.amountPerPerson("show");
+            calc.tipPerPerson("show");
+
+        }
+
+
+    }
+
+    // method to handle all the exceptions of decrement buttons 
+    static buttonException(id) {
+        let target = document.getElementById(id);
+
+        if (Number(target.value) == 1 && id == "number") {
+
+            let line = document.getElementById("numberException");
+            line.style.display = "block";
+            line.style.color = "red";
+            line.style.fontSize = "80%";
+            line.innerHTML = "Atleast one member should be there";
+
+
+
+        }
+        else if (Number(target.value) == 0) {
+            let line = document.getElementById(`${id}+Exception`);
+            line.style.display = "block";
+            line.style.color = "red";
+            line.style.fontSize = "80%";
+            line.innerHTML = "Number Should be +ve in value ";
+
+        }
+        else {
+            (target.value)--;
+
             calc.amountPerPerson("show");
             calc.tipPerPerson("show");
 
